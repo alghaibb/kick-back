d147958d81637eb31a183f8c69b65226d4c5651d
Moudi <mahmoud_jaderi@codewithmj.com>
2025-07-10 15:55:22 +1000

    Refactor EditEventModal to show the time user picked when creating the event

--- src/app/(main)/events/_components/EditEventModal.tsx
+++ src/app/(main)/events/_components/EditEventModal.tsx
@@ -9,19 +9,18 @@ import {
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal";
 import EditEventForm from "../forms/EditEventForm";
-import {format} from "date-fns";
+import { format } from "date-fns";
 
 export default function EditEventModal() {
   const { type, isOpen, close, data } = useModal();
   if (type !== "edit-event" || !data?.eventId || !data?.groups) return null;
 
-  // Prepare initialValues for the form
   const initialValues = {
     name: data.name ?? "",
     description: data.description ?? "",
     location: data.location ?? "",
     date: data.date ? new Date(data.date) : new Date(),
-    time: data.time ?? (data.date ? format(new Date(data.date), "HH:mm") : ""),
+    time: data.date ? format(new Date(data.date), "HH:mm") : "",
     groupId: data.groupId ?? undefined,
   };
 
