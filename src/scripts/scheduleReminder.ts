e7a8296cd55b23c20e4c696164bf0ef0bc40fe32
Halla Hassan Kuku <halla@Hallas-MBP.lan>
2025-07-14 01:16:29 +1000

    fix unused var error

--- src/scripts/scheduleReminder.ts
+++ src/scripts/scheduleReminder.ts
@@ -4,6 +4,7 @@ import { sendEventReminderEmail } from "@/utils/sendEmails";
 import { sendSMS } from "@/utils/sendSMS";
 import { formatToE164 } from "@/utils/formatPhoneNumber";
 import { addDays, startOfDay, endOfDay, format } from "date-fns";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { toZonedTime, format as formatTz } from "date-fns-tz";
 import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
 
