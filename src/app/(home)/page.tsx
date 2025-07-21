9f1b1faaa47dd74321dee3d569b07f7fbc669340
Moudi <mahmoud_jaderi@codewithmj.com>
2025-07-21 20:04:44 +1000

    remove all frontpage comps to start fresh

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,3 @@
-import Features from "./_components/Features";
-import HeroDesktop from "./_components/hero/HeroDesktop";
-import HowItWorks from "./_components/HowItWorks";
-
 export default function Home() {
-  return (
-    <>
-      <HeroDesktop />
-      <HowItWorks />
-      <Features />
-    </>
-  );
+  return <></>;
 }
